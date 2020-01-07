9d5ded18235aa649bec81c196480131636b17a11
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-01-07 09:15:00 +0100

    MultiStore Fix - add the possibility to save categories with same category key in different stores

---
+++ src/FondOfSpryker/Zed/Category/Communication/CategoryCommunicationFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace FondOfSpryker\Zed\Category\Communication;
+
+use FondOfSpryker\Zed\Category\CategoryDependencyProvider;
+use FondOfSpryker\Zed\Category\Communication\Form\CategoryType;
+use FondOfSpryker\Zed\Category\Dependency\Facade\CategoryToStoreBridge;
+use Generated\Shared\Transfer\CategoryTransfer;
+use Spryker\Zed\Category\Communication\CategoryCommunicationFactory as SprykerCategoryCommunicationFactory;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * @method \Spryker\Zed\Category\Persistence\CategoryQueryContainerInterface getQueryContainer()
+ * @method \Spryker\Zed\Category\CategoryConfig getConfig()
+ * @method \FondOfSpryker\Zed\Category\Business\CategoryFacadeInterface getFacade()
+ * @method \Spryker\Zed\Category\Persistence\CategoryRepositoryInterface getRepository()
+ */
+class CategoryCommunicationFactory extends SprykerCategoryCommunicationFactory
+{
+    /**
+     * @return \FondOfSpryker\Zed\Category\Dependency\Facade\CategoryToStoreBridge
+     */
+    public function getStoreFacade(): CategoryToStoreBridge
+    {
+        return $this->getProvidedDependency(CategoryDependencyProvider::FACADE_STORE);
+    }
+
+    /**
+     * @param int|null $idParentNode
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createCategoryCreateForm($idParentNode)
+    {
+        $categoryCreateDataFormProvider = $this->createCategoryCreateFormDataProvider();
+        $formFactory = $this->getFormFactory();
+
+        return $formFactory->create(
+            CategoryType::class,
+            $categoryCreateDataFormProvider->getData($idParentNode),
+            $categoryCreateDataFormProvider->getOptions()
+        );
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CategoryTransfer $categoryTransfer
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createCategoryEditForm(CategoryTransfer $categoryTransfer): FormInterface
+    {
+        $categoryCreateDataFormProvider = $this->createCategoryEditFormDataProvider();
+        $formFactory = $this->getFormFactory();
+
+        return $formFactory->create(
+            CategoryType::class,
+            $categoryTransfer,
+            $categoryCreateDataFormProvider->getOptions()
+        );
+    }
+}
---
+++ src/FondOfSpryker/Zed/Category/Communication/Form/CategoryType.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace FondOfSpryker\Zed\Category\Communication\Form;
+
+use Generated\Shared\Transfer\CategoryTransfer;
+use Spryker\Zed\Category\Communication\Form\CategoryType as SprykerCategoryType;
+use Spryker\Zed\Category\Persistence\CategoryQueryContainerInterface;
+use Spryker\Zed\PropelOrm\Business\Runtime\ActiveQuery\Criteria;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+/**
+ * @method \FondOfSpryker\Zed\Category\Business\CategoryFacadeInterface getFacade()
+ * @method \FondOfSpryker\Zed\Category\Communication\CategoryCommunicationFactory getFactory()
+ * @method \Spryker\Zed\Category\Persistence\CategoryQueryContainerInterface getQueryContainer()
+ * @method \Spryker\Zed\Category\CategoryConfig getConfig()
+ * @method \Spryker\Zed\Category\Persistence\CategoryRepositoryInterface getRepository()
+ */
+class CategoryType extends SprykerCategoryType
+{
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param \Spryker\Zed\Category\Persistence\CategoryQueryContainerInterface $categoryQueryContainer
+     *
+     * @return $this
+     */
+    protected function addCategoryKeyField(FormBuilderInterface $builder, CategoryQueryContainerInterface $categoryQueryContainer)
+    {
+        $storeId = $this->getFactory()->getStoreFacade()->getCurrentStore()->getIdStore();
+        $builder->add(static::FIELD_CATEGORY_KEY, TextType::class, [
+            'constraints' => [
+                new NotBlank(),
+                new Callback([
+                    'callback' => function ($key, ExecutionContextInterface $context) use ($categoryQueryContainer, $storeId) {
+                        $data = $context->getRoot()->getData();
+
+                        $exists = false;
+                        if ($data instanceof CategoryTransfer) {
+                            $exists = $categoryQueryContainer
+                                    ->queryCategoryByKey($key)
+                                    ->filterByIdCategory($data->getIdCategory(), Criteria::NOT_EQUAL)
+                                    ->filterByFkStore($storeId)
+                                    ->count() > 0;
+                        }
+
+                        if ($exists) {
+                            $context->addViolation(sprintf('Category with key "%s" already in use, please choose another one.', $key));
+                        }
+                    },
+                ]),
+            ],
+        ]);
+
+        return $this;
+    }
+}
